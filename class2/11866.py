19692283bd19a0b016ee4aca23a4c97a69bb1299
DESKTOP-EEFMB1L\USER <qkrwhdgur0319@gmail.com>
2025-05-14 17:51:25 +0900

    no message

--- class2/11866.py
+++ class2/11866.py
@@ -1,4 +1,5 @@
-N, K = map(int,input().split(""))
+
+N, K = map(int,input().split(" "))
 index = 0
 array = list(range(1,N+1))
 result = []
@@ -8,7 +9,6 @@ while len(array) != 0:
     index = index % len(array)
     result.append(array.pop(index))
 
-## 문자
 print("<",end="")
 for i in range(N-1):
     print(result[i],end=", ")
