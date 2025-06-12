15c52483c7e999c28065546191d0e54b565c76f4
DESKTOP-EEFMB1L\USER <qkrwhdgur0319@gmail.com>
2025-06-12 16:16:56 +0900

    no message

--- class3/1697.py
+++ class3/1697.py
@@ -2,11 +2,29 @@ from collections import deque
 
 n, k = map(int, input().split(" "))
 
-arr = deque()
-arr.append(5)
+time = 0
+time_limit = k - n
 
-end = n * ((k // n) + 1)
+arr = [0 for i in range(100001)]
 
+queue = deque()
+queue.append(n)
 
-def bfs():
-    print("asd")
+arr[n] = 1
+
+while queue:
+    a = queue.popleft()
+
+    if a == k:
+        print(arr[a] - 1)  # 결과 출력 시 -1 보정
+        break
+
+    if a + 1 <= 100000 and arr[a + 1] == 0:
+        arr[a + 1] = arr[a] + 1
+        queue.append(a + 1)
+    if a - 1 >= 0 and arr[a - 1] == 0:
+        arr[a - 1] = arr[a] + 1
+        queue.append(a - 1)
+    if a * 2 <= 100000 and arr[a * 2] == 0:
+        arr[a * 2] = arr[a] + 1
+        queue.append(a * 2)
