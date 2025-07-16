6c062c23c4333ec9b1ce91614d3d03d09bcea6d3
DESKTOP-EEFMB1L\USER <qkrwhdgur0319@gmail.com>
2025-07-16 17:35:09 +0900

    no message

--- DP1/1309.py
+++ DP1/1309.py
@@ -2,8 +2,7 @@ n = int(input())
 arr = [1] * (n + 1)
 
 
-
 for i in range(1, n + 1):
-    arr[i] = (2 * arr[i - 1] + arr[i - 2])% 9901
+    arr[i] = (2 * arr[i - 1] + arr[i - 2]) % 9901
 
 print(arr[n])
